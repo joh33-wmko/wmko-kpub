581ef66a6e9185e02c3f3f8036e8bd0468987ed2
Josh Riley <joshjriley@gmail.com>
2021-05-05 16:18:40 -1000

    add keck legacy export of instrument and archive columns

--- data/keck/export_legacy_data.py
+++ data/keck/export_legacy_data.py
@@ -6,9 +6,20 @@ df = df.fillna('')
 newdata = []
 for idx, row in df.iterrows():
 	bibcode = row['BibCode']
+
 	if not bibcode:
 		continue
-	data = {'bibcode':bibcode, 'mission':'keck', 'science':''}
+
+	tac = row['TAC'].upper()
+	archive = 1 if 'KOA' in tac else 0
+
+	data = {
+		'bibcode':bibcode, 
+		'mission':'keck', 
+		'science':'', 
+		'instruments':row['Instru'],
+		'archive':archive
+	}
 	newdata.insert(0, data)
 
 #write out all for use with kpub import 
